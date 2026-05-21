f1e8b275974be51ba59d0cf55c38269497e7ef9d
Jonathan Krall <jonathan.krall@lexicon.se>
2026-05-21 13:24:19 +0200

    Removed robot-things from Program.cs

--- PersonalRegister2026VT/Program.cs
+++ PersonalRegister2026VT/Program.cs
@@ -4,20 +4,7 @@
     {
         static void Main(string[] args)
         {            
-            Robot r1 = new Robot("R1", 25, 25000);
-            //r1.Count;
-            //Robot.Count; 
-            Console.WriteLine(Robot.Count);
-            Robot r2 = new Robot("R2", 25, 25000);
-            Console.WriteLine(Robot.Count);
-            Robot r3 = new Robot("R3", 25, 25000);
-            Console.WriteLine(Robot.Count);
-            Robot r4 = new Robot("R4", 25, 25000);
-            Console.WriteLine(Robot.Count);
-            Robot r5 = new Robot("R5", 25, 25000);
-            Console.WriteLine(Robot.Count);
-
-            Console.ReadKey();
+           
         }
     }
 }
