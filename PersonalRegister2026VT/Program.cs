afe576e6459b49dbb1c71869b48309444804f454
Jonathan Krall <jonathan.krall@lexicon.se>
2026-05-21 14:01:39 +0200

    Added some seed data

--- PersonalRegister2026VT/Program.cs
+++ PersonalRegister2026VT/Program.cs
@@ -2,12 +2,26 @@
 {
     internal class Program
     {
+        private static Payroll payroll = new Payroll();        
         static void Main(string[] args)
         {
             //Employee emp = new Employee("Kalle", 25000);
-            Payroll payroll = new Payroll();
+            //Payroll payroll = new Payroll();
 
-            payroll.AddEmployee("Nisse", 30000); 
+
+            SeedData();
+
+            Console.ReadKey();
+        }
+
+        private static void SeedData()
+        {
+            payroll.AddEmployee("Nisse", 30000);            
+            payroll.AddEmployee("Anna", 35000);            
+            payroll.AddEmployee("Stig", 40000);            
+            payroll.AddEmployee("Stina", 45000);            
+            payroll.AddEmployee("Anders", 50000);            
+            payroll.AddEmployee("Åsa", 55000);            
         }
     }
 }
