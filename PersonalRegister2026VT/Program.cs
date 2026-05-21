f1e472a293935b2cc500ad0eb48645a82b810588
Jonathan Krall <jonathan.krall@lexicon.se>
2026-05-21 11:58:34 +0200

    Added some robot stuff to show properties/fields/methods

--- PersonalRegister2026VT/Program.cs
+++ PersonalRegister2026VT/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Please enter a name for your robot: ");
+            string name = Console.ReadLine();
+
+            Robot robot = new Robot(name, 67, 25000);
+            //robot._name = "C3PO";
+            //robot.SetName("C3PO");
+            robot.Age = 1;
+            //robot.Salary = 25000;
+            Console.WriteLine(robot.GetName());
+            Console.WriteLine(robot.Age);
+            Console.WriteLine(robot.Salary);
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ PersonalRegister2026VT/Robot.cs
@@ -0,0 +1,47 @@
+﻿namespace PersonalRegister2026VT
+{
+    internal class Robot
+    {
+        private string _name;
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 0)
+                    _age = 0;
+                else
+                _age = value;
+            }
+        }
+
+        //public int Salary { get; private set; }
+        public int Salary { get;}
+
+
+        public Robot(string name, int age, int salary)
+        {
+            _name = name;
+            Age = age;
+            Salary = salary; 
+        }
+
+        public void SetSalary(int salary)
+        {
+            //Salary = salary; 
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            //Validation
+            _name = name;
+        }
+    }
+}
