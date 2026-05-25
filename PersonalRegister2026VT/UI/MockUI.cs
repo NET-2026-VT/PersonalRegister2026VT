e07135c75a809c06acd0bd6033238f66fcff737e
Jonathan Krall <jonathan.krall@lexicon.se>
2026-05-25 15:34:35 +0200

    Added some simple test.

--- PersonalRegister2026VT/UI/MockUI.cs
+++ PersonalRegister2026VT/UI/MockUI.cs
@@ -7,9 +7,10 @@ namespace PersonalRegister2026VT.UI
 {
     public class MockUI : IUI
     {
+        public string SetInput { private get; set; } = "1"; 
         public string GetInput()
         {
-            return "1";
+            return SetInput;
         }
 
         public void Print(string message)
---
+++ PersonalRegister2026VTTests/UtilTests.cs
@@ -0,0 +1,21 @@
+﻿using PersonalRegister2026VT.Helpers;
+using PersonalRegister2026VT.UI;
+
+namespace PersonalRegister2026VTTests
+{
+    public class UtilTests
+    {
+        [Fact]
+        public void AskForString_ShouldReturnExpectedString()
+        {
+            //Arrange
+            const string expected = "some value";
+            var mockUI = new MockUI();
+            mockUI.SetInput = expected;
+            //Act
+            var actual = Util.AskForString("", mockUI);
+            //Assert
+            Assert.Equal(expected, actual); 
+        }
+    }
+}
