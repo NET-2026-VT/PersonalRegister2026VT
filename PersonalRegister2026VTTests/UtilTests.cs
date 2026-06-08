128a300f03b4836a664664fc992f68fc74cb2f4f
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-08 12:01:41 +0200

    Moq

--- PersonalRegister2026VTTests/UtilTests.cs
+++ PersonalRegister2026VTTests/UtilTests.cs
@@ -1,4 +1,6 @@
-﻿using PersonalRegister2026VT.Helpers;
+﻿using Moq;
+using PersonalRegister2026VT.Abstractions;
+using PersonalRegister2026VT.Helpers;
 using PersonalRegister2026VT.UI;
 
 namespace PersonalRegister2026VTTests
@@ -14,6 +16,19 @@ namespace PersonalRegister2026VTTests
             mockUI.SetInput = expected;
             //Act
             var actual = Util.AskForString("", mockUI);
+            //Assert
+            Assert.Equal(expected, actual); 
+        }
+        
+        [Fact]
+        public void AskForString_ShouldReturnExpectedString_WithMoq()
+        {
+            //Arrange
+            const string expected = "some value";
+            var mockUI = new Mock<IUI>();
+            mockUI.Setup(ui => ui.GetInput()).Returns(expected);
+            //Act
+            var actual = Util.AskForString("", mockUI.Object);
             //Assert
             Assert.Equal(expected, actual); 
         }
